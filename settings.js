82a7675c543d977280937c4799acd4f05573a34c
Alfanbot9 <112259665+Alfanbot9@users.noreply.github.com>
2022-08-27 20:56:00 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,105 +1,139 @@
-/*
-› Create By @RIY
-› Base Ori @DikaArdnt
-› Kalau mau tambahin fitur di index.js
-› Edit text menu di file language › help.js
-*/
-
-//gausah di apa² in!
 const fs = require('fs')
 const chalk = require('chalk')
+const  { indonesia, english, spanyol} = require(`./language`)
 
-//—————「 Website Apikey 」—————//
+// Website Api (jgn di ganti tomlol)
 global.APIs = {
-//gausah di apa² in!
-zenz: 'https://zenzapis.xyz',
+	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan ke owner
 }
 
-//—————「 Website Apikey 」—————//
+// Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-//gausah di apa² in, udah free apikey :)
-'https://zenzapis.xyz': 'BagasGanz',
+	'https://api.zeeoneofc.xyz': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
-//—————「 Setting Owner 」—————//
-global.owner = ['6285267480703','6285267480703','6285267480703'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6285267480703'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6285267480703' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+// setting 
+global.autoread = false // auto read message
+global.autobio = true 
+global.userRegister = false 
+global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
-//—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'SANZ-MD' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'SANZ-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'SANZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//language
+//Available in indonesia , english & spanyol 
+//You can request others language 
+global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
 
-//—————「 Set Kebutuhan Button & Kontak 」—————//
-//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
-global.region = 'indonesia'
 
-//—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-
-//—————「 Set Nama Session 」—————//
-//gausah di apa² in!
+// Other
+// Other
+global.botname = "Alphabot-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
+global.email = "zeeoneofc@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.timezone = 'Asia/Jakarta' //  timezone wib
+global.premium = ['62887435047326'] //premium user
+global.owner = ["6285342106390","622150996855"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "62887435047326" // nomor wa kalian
+global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
+global.packname = '© Alphabot-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
-
-//—————「 Set Limit 」—————//
-//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
+global.typemenu = 'document'
+global.multiplier = 69
 global.limitawal = {
-premium: "Infinity",
-free: 15,
+    premium: "10000",
+    free: 100
 }
 
-//—————「 Set Image 」—————//
-//terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.thumb = fs.readFileSync('./image/chika.jpg')
-/*global.veloriy = fs.readFileSync('./image/chika.mp4')*/
+global.adventureRPG = 'https://telegra.ph/file/ff94536d69e0f4f3e7b54.jpg'
+global.bankRPG = 'https://telegra.ph/file/ce47dca98cfdb34162abb.png'
+global.bansosRPG = 'https://telegra.ph/file/646af753f38fbf7bbbccc.jpg'
+global.bansosRPG_ = 'https://telegra.ph/file/ba18a18aac022170c8056.jpg'
+global.berburuRPG = 'https://telegra.ph/file/95a202f631ab7172c3f25.jpg'
+global.inventoryRPG = 'https://telegra.ph/file/507395e2edbd1d740a0fd.png'
+global.cooldownRPG = 'https://telegra.ph/file/5e1e191a47028d9648ffa.jpg'
+global.dailyRPG = 'https://telegra.ph/file/17e857848a3afdb1c4fd8.jpg'
+global.hourlyRPG = 'https://telegra.ph/file/bcabf71c0f56207d1629e.jpg'
+global.kandangRPG = 'https://telegra.ph/file/89c3b3ec69d373da8f64b.jpg'
+global.kolamRGP = 'https://telegra.ph/file/0bc07117c78b2c9674b7d.jpg'
+global.mancingRPG = 'https://telegra.ph/file/1420d26b7f04f84993225.jpg'
+global.monthlyRPG = 'https://telegra.ph/file/7eadf65b95d9f7a3ecb46.jpg'
+global.weeklyRPG = 'https://telegra.ph/file/ffc85895d14db97931f0f.jpg'
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '📊',
+      limit: '🎫',
+      health: '❤️',
+      exp: '✨',
+      money: '💹',
+      bank: '🏦',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🛍️',
+      mythic: '🎁',
+      legendary: '🗃️',
+      superior: '💼',
+      pet: '🔖',
+      trash: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      pickaxe: '⛏️',
+      fishingrod: '🎣',
+      wood: '🪵',
+      rock: '🪨',
+      string: '🕸️',
+      horse: '🐴',
+      cat: '🐱',
+      dog: '🐶',
+      fox: '🦊',
+      petFood: '🍖',
+      iron: '⛓️',
+      gold: '🪙',
+      emerald: '❇️',
+      upgrader: '🧰'
+      
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
 
-//—————「 Set Image Welcome 」—————//
-//terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
-
-//—————「 Set Random Image Menu 」—————//
-//gausah di apa² in!
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-//—————「 Set Prefix 」—————//
-//gausah di apa² in!
 global.prefa = ['','!','.','🐦','🐤','🗿']
-
-//—————「 Set Simbol 」—————//
-//terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
 global.sp = '⭔'
-
-//—————「 Set Message 」—————//
-//terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
 global.mess = {
-success: '_Done._',
-admin: '_Fitur Khusus Admin Group!_',
-botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
-owner: '_Fitur Khusus Owner Bot_',
-group: '_Fitur Digunakan Hanya Untuk Group!_',
-private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
-bot: '_Fitur Khusus Pengguna Nomor Bot_',
-wait: '_Sedang Di Proses..._',
-error: '_Fitur Sedang Error!_',
-endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
-//—————「 Batas Akhir 」—————//
-//gausah si apa² in!
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(chalk.redBright(`Update'${__filename}'`))
-delete require.cache[file]
-require(file)
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
 })
